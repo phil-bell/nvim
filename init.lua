c7af0147936d0cb2caac3b3b5a62fe009045c72e
Phil Bell <philip.bell@kraken.tech>
2026-01-02 12:51:52 +0000

    Add harpoon setup

--- init.lua
+++ init.lua
@@ -5,3 +5,27 @@ require("kraken").setup()
 --require("mason-lspconfig").setup({
 --  ensure_installed = { "pyrefly" },
 --})
+local harpoon = require("harpoon")
+local harpoon_extensions = require("harpoon.extensions")
+harpoon:extend(harpoon_extensions.builtins.highlight_current_file())
+local harpoon_files = require("harpoon_files")
+local icons = LazyVim.config.icons
+require("lualine").setup({
+  sections = {
+    lualine_c = {
+      LazyVim.lualine.root_dir(),
+      {
+        "diagnostics",
+        symbols = {
+          error = icons.diagnostics.Error,
+          warn = icons.diagnostics.Warn,
+          info = icons.diagnostics.Info,
+          hint = icons.diagnostics.Hint,
+        },
+      },
+      { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+      { LazyVim.lualine.pretty_path({ length = 10 }) },
+      harpoon_files.lualine_component,
+    },
+  },
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,7 +4,6 @@
 -- LSP Server to use for Python.
 -- Set to "basedpyright" to use basedpyright instead of pyright.
 -- Set to "ruff_lsp" to use the old LSP implementation version.
-
 vim.g.lazyvim_python_lsp = "pyright"
 vim.env.PYTHONPATH = "/Users/philip.bell/projects/kraken-core/src"
 vim.g.test_cmd = { "-p tools.pytest.django_config_auto_selection.plugin", "-vvv" }
