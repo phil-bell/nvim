c95639dda2ca11573efe35a3456780b4d44cf3be
Phil Bell <philip.bell@kraken.tech>
2026-02-11 18:02:18 +0000

    Add keyboard shortcut for copy path and copy test command

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,47 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
+
+map("n", "<leader>fC", function()
+  local path = vim.fn.expand("%:p") --[[@as string]]
+
+  if path == "" then
+    return ""
+  end
+
+  path = LazyVim.norm(path)
+  local root = LazyVim.root.get({ normalize = true })
+  local cwd = LazyVim.root.cwd()
+  local norm_path = path
+
+  if norm_path:find(cwd, 1, true) == 1 then
+    path = path:sub(#cwd + 2)
+  elseif norm_path:find(root, 1, true) == 1 then
+    path = path:sub(#root + 2)
+  end
+
+  vim.fn.setreg("+", path)
+end, { desc = "Copy relative path" })
+
+map("n", "<leader>tc", function()
+  local path = vim.fn.expand("%:p") --[[@as string]]
+
+  if path == "" then
+    return ""
+  end
+
+  path = LazyVim.norm(path)
+  local root = LazyVim.root.get({ normalize = true })
+  local cwd = LazyVim.root.cwd()
+  local norm_path = path
+
+  if norm_path:find(cwd, 1, true) == 1 then
+    path = path:sub(#cwd + 2)
+  elseif norm_path:find(root, 1, true) == 1 then
+    path = path:sub(#root + 2)
+  end
+  local func = vim.fn.expand("<cword>")
+
+  vim.fn.setreg("+", "inv localdev.pytest " .. path .. " -- -k " .. func)
+end, { desc = "Copy relative path" })
