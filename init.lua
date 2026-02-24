ecc5240009f731c488f494113f182943bda21bee
Phil Bell <philip.bell@kraken.tech>
2026-02-24 16:21:59 +0000

    comment hard coding of path length

--- init.lua
+++ init.lua
@@ -12,20 +12,20 @@ local harpoon_files = require("harpoon_files")
 local icons = LazyVim.config.icons
 require("lualine").setup({
   sections = {
-    lualine_c = {
-      LazyVim.lualine.root_dir(),
-      {
-        "diagnostics",
-        symbols = {
-          error = icons.diagnostics.Error,
-          warn = icons.diagnostics.Warn,
-          info = icons.diagnostics.Info,
-          hint = icons.diagnostics.Hint,
-        },
-      },
-      { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-      { LazyVim.lualine.pretty_path({ length = 10 }) },
-    },
+    -- lualine_c = {
+    --   LazyVim.lualine.root_dir(),
+    --   {
+    --     "diagnostics",
+    --     symbols = {
+    --       error = icons.diagnostics.Error,
+    --       warn = icons.diagnostics.Warn,
+    --       info = icons.diagnostics.Info,
+    --       hint = icons.diagnostics.Hint,
+    --     },
+    --   },
+    --   { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+    --   { LazyVim.lualine.pretty_path({ length = 10 }) },
+    -- },
     lualine_x = {
       harpoon_files.lualine_component,
       Snacks.profiler.status(),
