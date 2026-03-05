a48b678f9032ebc9f473f8cce8074164b36c31b0
Phil Bell <philip.bell@kraken.tech>
2026-03-05 17:57:25 +0000

    move path fetching to reusable function

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,7 +3,7 @@
 -- Add any additional keymaps here
 local map = LazyVim.safe_keymap_set
 
-map("n", "<leader>fC", function()
+function getPath()
   local path = vim.fn.expand("%:p") --[[@as string]]
 
   if path == "" then
@@ -20,27 +20,16 @@ map("n", "<leader>fC", function()
   elseif norm_path:find(root, 1, true) == 1 then
     path = path:sub(#root + 2)
   end
+  return path
+end
 
+map("n", "<leader>fC", function()
+  local path = getPath()
   vim.fn.setreg("+", path)
 end, { desc = "Copy relative path" })
 
 map("n", "<leader>tc", function()
-  local path = vim.fn.expand("%:p") --[[@as string]]
-
-  if path == "" then
-    return ""
-  end
-
-  path = LazyVim.norm(path)
-  local root = LazyVim.root.get({ normalize = true })
-  local cwd = LazyVim.root.cwd()
-  local norm_path = path
-
-  if norm_path:find(cwd, 1, true) == 1 then
-    path = path:sub(#cwd + 2)
-  elseif norm_path:find(root, 1, true) == 1 then
-    path = path:sub(#root + 2)
-  end
+  local path = getPath()
   local func = vim.fn.expand("<cword>")
 
   vim.fn.setreg("+", "inv localdev.pytest " .. path .. " -- -k " .. func)
