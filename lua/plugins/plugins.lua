d4c80bfa62b616fad2a8ed1922c219e45c03c414
Phil Bell <philip.bell@kraken.tech>
2026-07-20 10:34:36 +0100

    simplifying config

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,49 +1,9 @@
 return {
-  {
-    "ohakutsu/socks-copypath.nvim",
-    config = function()
-      require("socks-copypath").setup()
-    end,
-  },
   { "bufferline.nvim", enabled = false },
-  { "akinsho/git-conflict.nvim", version = "*", config = true },
-  {
-    "kiennt63/harpoon-files.nvim",
-    dependencies = {
-      { "ThePrimeagen/harpoon", branch = "harpoon2" },
-    },
-  },
   {
     "octoenergy/kraken.nvim",
     opts = { commands = {} },
     dev = true,
-  },
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "nvim-neotest/nvim-nio",
-      "nvim-lua/plenary.nvim",
-      "antoinemadec/FixCursorHold.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-      require("neotest").setup({
-        adapters = {
-          require("neotest-python")({
-            dap = { justMyCode = true, django = true },
-            args = { "-p tools.pytest.django_config_auto_selection.plugin", "-vvv" },
-          }),
-        },
-        output = { open_on_run = true },
-        discovery = {
-          enabled = false,
-          concurrent = 1,
-        },
-        summary = {
-          animated = false,
-        },
-      })
-    end,
   },
   {
     {
@@ -51,22 +11,6 @@ return {
       opts = {
         cmdline = {
           view = "cmdline",
-        },
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        basedpyright = {
-          settings = {
-            basedpyright = {
-              analysis = {
-                typeCheckingMode = "standard",
-              },
-            },
-          },
         },
       },
     },
